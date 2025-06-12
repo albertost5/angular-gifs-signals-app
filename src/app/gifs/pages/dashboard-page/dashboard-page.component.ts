1477f5c7d10821a3fb31e6123e9e0f6702891739
albertost5 <sancheztorresalberto@gmail.com>
2025-06-12 11:22:27 +0200

    Added tailwind css integration and gifs functionality

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './dashboard-page.component.html',
+  standalone: true,
+  styles: ``
+})
+export default class DashboardPageComponent {
+
+}
---
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page',
+  imports: [],
+  templateUrl: './search-page.component.html',
+  standalone: true,
+  styles: ``
+})
+export default class SearchPageComponent {
+
+}
